2a2f31e1c39dc8c78ad4f2b3ff80395699bdf26e
The Captaan <thecaptaan@proton.me>
2024-02-09 23:03:12 +0530

    add output & debug function

---
+++ 02 output.php
@@ -0,0 +1,18 @@
+<?php
+
+// echo() - print single or multiple values
+echo 'Hello, world!', 123;
+
+// print() - only print single value
+print 123;
+
+// print_r() - print single or values of array
+print_r('2','3');
+
+// var_dump() - print single or multiple values with data type
+var_dump(0);
+
+// var_export() - similar to var_dump() but it returns the output a string rep of the variable
+var_export(1);
+
+?>
